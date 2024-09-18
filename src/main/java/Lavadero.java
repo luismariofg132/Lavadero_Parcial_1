4e1addcadfe45bdbf3e7d34388f7caf43d6227f2
Luis Franco <lmfg06@gmail.com>
2024-09-18 00:48:35 -0500

    Proyecto Finalizado

---
+++ src/main/java/Lavadero.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author lmfg0
+ */
+public class Lavadero {
+
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        menu.setVisible(true);
+    }
+}
---
+++ src/main/java/Vehiculo.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lmfg0
+ */
+public class Vehiculo {
+    private String placa;
+    private String propietario;
+    private tipoVehiculo vehiculo;
+    private tipoLavado lavado;
+    private boolean finalizado;
+    private double ganancia;
+    private double costo;
+
+    public double getGanancia() {
+        return ganancia;
+    }
+
+    public void setGanancia(double ganancia) {
+        this.ganancia = ganancia;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+    
+    public Vehiculo(){
+        this.costo = 0;
+        this.ganancia = 0;
+        this.finalizado = false;
+    }
+    
+    public Vehiculo(String placa, String propietario, tipoVehiculo vehiculo, tipoLavado lavado, boolean finalizado, double ganancia, double costo) {
+        this.placa = placa;
+        this.propietario = propietario;
+        this.vehiculo = vehiculo;
+        this.lavado = lavado;
+        this.finalizado = finalizado;
+        this.ganancia = ganancia;
+        this.costo = costo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
+    public tipoVehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(tipoVehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public tipoLavado getLavado() {
+        return lavado;
+    }
+
+    public void setLavado(tipoLavado lavado) {
+        this.lavado = lavado;
+    }
+
+    public boolean isFinalizado() {
+        return finalizado;
+    }
+
+    public void setFinalizado(boolean finalizado) {
+        this.finalizado = finalizado;
+    }
+}
